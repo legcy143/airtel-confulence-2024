534d201fed503bb2df1af41cd1c7aa51d5c1ce8d
prince <legcy143@gmail.com>
2024-11-09 15:26:28 +0530

    fix: add table locater

--- src/app/client/searchseat/page.tsx
+++ src/app/client/searchseat/page.tsx
@@ -3,8 +3,19 @@ import { EventDetail } from "@/constants/EventDetail";
 import { Button, Divider, Input, Tab, Tabs } from "@nextui-org/react";
 import React from "react";
 import { Icon } from "@iconify/react";
+import ShowDataTable from "./ShowDataTable";
 
 export default function page() {
+  const data = [
+    {
+      _id:1,
+      seq: 1,
+      name: "John",
+      seatNumber:4,
+      tableNumber:2,
+    },
+  ];
+
   return (
     <main>
       <nav className="px-1 md:px-5 border-b-0 h-[5rem] flex items-center justify-between sticky top-0 backdrop-blur-xl z-30">
@@ -18,14 +29,15 @@ export default function page() {
         </div>
       </nav>
       <Divider />
-      <main className="p-2 max-w-[50rem] mx-auto">
+      <main className="p-2 max-w-[50rem] mx-auto space-y-3">
         <Input
           type="search"
           startContent={<Icon icon="ic:round-search" />}
-          label="Search seat"
-          description="search your seat by email or name "
+          label="Search Seat"
+          description="Search your seat by email or name "
           placeholder="Name or Email ..."
         />
+        <ShowDataTable data={data} />
       </main>
     </main>
   );
